19368f9bbb9935a50eb8ccdccc1b7d7efe11f647
Antonio Placido <aplacido1992@gmail.com>
2020-08-18 15:29:00 -0400

    dont forget the S in PARAMS

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -11,16 +11,17 @@ const options = {
 };
 
 const getGreeting = async (req, res) => {
-  let _id = req.param._id;
+  let _id = req.params._id;
 
   const client = await MongoClient(MONGO_URI, options);
   await client.connect();
   const db = client.db("exercise_1");
-
+  console.log({ _id });
   db.collection("greetings").findOne({ _id }, (err, result) => {
     result
       ? res.status(200).json({ status: 200, _id, data: result })
       : res.status(404).json({ status: 404, _id, data: "Not Found" });
+    console.log(_id);
     client.close();
   });
 };
